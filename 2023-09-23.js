5a1f0874b822b2d66b06991da453b0ea5f114d74
Rute Henriques <rutehenriques@outlook.com>
2023-09-23 09:51:27 +0200

    Added an additional problem

--- 2023-09-23.js
+++ 2023-09-23.js
@@ -6,4 +6,13 @@ function simpleMultiplication(number) {
     }else{
       return 9 * number;
     }
-}echo
+}
+
+
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name) {
+  return 'Hello, ' + name
+}
+sayHello()

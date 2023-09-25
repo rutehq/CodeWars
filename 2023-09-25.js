4e64a5248df1e613a27da1ae655cedc1ac5e5aee
Rute Henriques <rutehenriques@outlook.com>
2023-09-25 16:37:42 +0200

    Daily codeWars

---
+++ 2023-09-25.js
@@ -0,0 +1,35 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+    return n % 2 === 0 ? true : false
+}
+
+
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    if((p1 === "scissors" && p2 === "paper")|| 
+       (p1 === "rock" && p2 === "scissors") || 
+       (p1 === "paper" && p2 === "rock" )){
+      return "Player 1 won!"
+    }else if(p1 === p2){
+      return "Draw!"
+    }else{
+      return "Player 2 won!"
+    }
+  };

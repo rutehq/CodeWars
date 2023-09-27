bc78ac1d16647643b68cd04c2646d0f9a5f7be67
Rute Henriques <rutehenriques@outlook.com>
2023-09-27 16:24:51 +0200

    Daily codeWars

---
+++ 2023-09-27.js
@@ -0,0 +1,26 @@
+//Build a function that returns an array of integers from n to 1 where n>0.
+
+//Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let newArray = [];
+   for(i=n; i > 0; i--){
+    newArray.push(i);
+    } return newArray;
+};
+ reverseSeq(5)
+
+
+//  Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+
+
+
+function squareDigits(num){
+    let digits = []
+    digits = num.split()
+    return 0;
+  }
+squareDigits(203)
+  console.log(digits)
